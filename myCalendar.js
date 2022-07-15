b09098df93e5cd5bf6ed8605f2478fa88adaa5f8
KedarGhadge <kghadge84@gmail.com>
2022-07-15 14:23:19 +0530

    color month datewise and open popup on date click

--- myCalendar.js
+++ myCalendar.js
@@ -1,18 +1,25 @@
 var myYear;
+this.aptDates = ['4','7'];
 this.year = document.getElementById("calendar-year");
 function CalendarApp(date) {
   if (!(date instanceof Date)) {
     date = new Date();
   }
 
-  //   this.showView(date);
-  //   this.addEventListeners();
   myYear = date.getFullYear();
   this.year.textContent = myYear;
   SetMonths(myYear);
 }
 
+// function display(test) {
+//   $("#dialog").display = "inline";
+//   $('#dialog').modal('show');
+// }
+
 function nextYear() {
+  // $('#popupUploadImages').display = "inline";
+  // $('#popupUploadImages').toggle();
+  // $('#popupUploadImages').modal('show');
   clearOldSection();
   //   document.querySelector('[id^="d-"]').id.textContent = "";
   myYear = myYear + 1;
@@ -81,7 +88,33 @@ function SetMonths(year) {
           ? (document.getElementById("d-sat1").textContent = toMonthName(i))
           : (document.getElementById("d-sat2").textContent = toMonthName(i));
     }
-    console.log(day);
+    
+    // console.log(day);
+  }
+  elements = document.getElementsByTagName("td")
+      for (var j = elements.length; j--;) {
+      if (elements[j].innerHTML === "Sun") {
+      elements[j].style.color = "red";
+    }
+    else if(elements[j].innerHTML === "31")
+    elements[j].style.color = "blue";
+    else if(elements[j].innerHTML === "30")
+    elements[j].style.color = "green";
+    else if(elements[j].innerHTML === checkLeapYear(year))
+    elements[j].style.color = "yellow";
+    if(this.aptDates.indexOf(elements[j].innerHTML) !== -1 ){
+    elements[j].style.display = 'block';
+    elements[j].style.fontWeight="bold";
+    }
+  }
+  elements = document.getElementsByTagName("th")
+      for (var j = elements.length; j--;) {
+      if (elements[j].innerHTML === "Jan" || elements[j].innerHTML === "Mar" || elements[j].innerHTML === "May" || elements[j].innerHTML === "Jul" || elements[j].innerHTML === "Aug" || elements[j].innerHTML === "Oct" || elements[j].innerHTML === "Dec") 
+      elements[j].style.color = "blue";
+      else if(elements[j].innerHTML === "Apr" || elements[j].innerHTML === "Jun" || elements[j].innerHTML === "Sep" || elements[j].innerHTML === "Nov" )
+      elements[j].style.color = "green";
+      else if(elements[j].innerHTML === "Feb")
+      elements[j].style.color = "yellow";
   }
 }
 
@@ -118,4 +151,49 @@ function clearOldSection() {
   document.getElementById("d-thu2").textContent = "";
   document.getElementById("d-fri2").textContent = "";
   document.getElementById("d-sat2").textContent = "";
+
+  elements = document.getElementsByTagName("th")
+      for (var j = elements.length; j--;) {
+      elements[j].style.color = "black";
+  }
+  elements = document.getElementsByTagName("td")
+      for (var j = elements.length; j--;) {
+      elements[j].style.color = "black";
+  }
 }
+
+function checkLeapYear(year) {
+  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+      return '29';
+  } else {
+      return '28';
+  }
+}
+
+
+function deselect(e) {
+  $('.pop').slideFadeToggle(function() {
+    e.removeClass('selected');
+  });    
+}
+
+$(function() {
+  $('.tg-0lax-day').on('click', function() {
+    if($(this).hasClass('selected')) {
+      deselect($(this));               
+    } else {
+      $(this).addClass('selected');
+      $('.pop').slideFadeToggle();
+    }
+    return false;
+  });
+
+  $('.close').on('click', function() {
+    deselect($('.tg-0lax-day'));
+    return false;
+  });
+});
+
+$.fn.slideFadeToggle = function(easing, callback) {
+  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
